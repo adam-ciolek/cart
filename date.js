e4d93993c0d979aeae6147b5c0d929f1ecf30d3b
Adam Ciołek <adasciolek@gmail.com>
2021-11-17 14:54:03 +0100

    Slider

--- date.js
+++ date.js
@@ -13,3 +13,11 @@ export const imgsSmallest =
     {id:7, img:'image-product-3-thumbnail.jpg'},
     {id:8, img:'image-product-4-thumbnail.jpg'},
 ]
+
+const imgsBiggest = 
+[
+    {id: 1, img:'image-product-1.jpg'},
+    {id: 2, img:'image-product-2.jpg'},
+    {id: 3, img:'image-product-3.jpg'},
+    {id: 4, img:'image-product-4.jpg'},
+]
--- slider.js
+++ slider.js
@@ -1,41 +1,124 @@
 
 const imgS = document.querySelectorAll('.shoes-img-small img');
 const imgB = document.querySelector('.shoes-img')
- 
 
 // slider first page
+const imgsBiggest = 
+[
+    {id: 0, img:'http://127.0.0.1:5500/images/image-product-1.jpg'},
+    {id: 1, img:'http://127.0.0.1:5500/images/image-product-2.jpg'},
+    {id: 2, img:'http://127.0.0.1:5500/images/image-product-3.jpg'},
+    {id: 3, img:'http://127.0.0.1:5500/images/image-product-4.jpg'},
+]
+
 imgS.forEach( img => {
-    img.addEventListener('click', e => {
-        const target = e.target.dataset.set
+    img.addEventListener("click", e =>{
+    
+        const targetDataSet = e.target.dataset.set
 
-        switch (target) {
-            case '0': 
+        imgS.forEach(item => {
+            item.classList.remove('is-active-focus')
+        })
+
+        imgsBiggest.find( el => {
+             if(el.id == targetDataSet) {
+                imgB.src = el.img 
                 img.classList.add('is-active-focus')
-                imgB.src= "images/image-product-1.jpg"
-            break;
-            case '1':  
-                imgB.src="images/image-product-2.jpg"
-            break;
-            case '2':
-                imgB.src="images/image-product-3.jpg"
-            break;
-            case '3':
-                imgB.src="images/image-product-4.jpg"
-            break;
-        }
-    })
+            }
+        });  
+    });
+});
 
-})
+// slide in shadow page 
+
+const imgSShadow = document.querySelectorAll('.shoes-img-small-shadow img');
+const imgBShadow = document.querySelector('.shoes-img-shadow');
+
+const arrowLeft = document.querySelector('.previous');
+const arrowRight = document.querySelector('.next');
+ 
+
+const imgsBiggestShadow = 
+[
+    {id: 4, img:'http://127.0.0.1:5500/images/image-product-1.jpg'},
+    {id: 5, img:'http://127.0.0.1:5500/images/image-product-2.jpg'},
+    {id: 6, img:'http://127.0.0.1:5500/images/image-product-3.jpg'},
+    {id: 7, img:'http://127.0.0.1:5500/images/image-product-4.jpg'},
+]
+
+imgSShadow.forEach( img => {
+    img.addEventListener("click", e =>{
+    
+        const targetDataSet = e.target.dataset.set
+
+        imgSShadow.forEach(item => {
+            item.classList.remove('is-active-focus')
+        })
+
+        imgsBiggestShadow.find( el => {
+             if(el.id == targetDataSet) {
+                imgBShadow.src = el.img 
+                img.classList.add('is-active-focus')
+            }
+        });  
+    });
+});
 
 
+let countImg = imgsBiggestShadow.length;
+let stopSliding = false
 
+const handleArrowLeft = () => {
 
+    stopSliding= true
+    if(stopSliding){ 
+        
+        let changeImg = '' 
+        
+        if(countImg >= 5) {
+            countImg--
+            stopSliding = false
+            imgsBiggestShadow.forEach( img => {
+                if(countImg == img.id) {
+                    changeImg = img.img   
+                }
+            })       
+        } else {
+            imgsBiggestShadow.forEach( img => {
+                if(countImg == img.id) {
+                    changeImg = img.img   
+                }
+            })
+        }    
+        imgBShadow.src = changeImg
+    }
+    console.log(countImg)
+}
 
+const handleArrowRight = () => {
 
+    stopSliding= true
+    
+    if(stopSliding){ 
+        let changeImg = '' 
+        if(countImg >= 7) {
+            stopSliding = false
+            imgsBiggestShadow.forEach( img => {
+                if(countImg == img.id) {
+                    changeImg = img.img   
+                }
+            })       
+        } else {
+            countImg++
+            imgsBiggestShadow.forEach( img => {
+                if(countImg == img.id) {
+                     changeImg = img.img   
+                }
+            })
+        }    
+        imgBShadow.src = changeImg
+    }
+}
 
-
-
-
-
-
-
+arrowLeft.addEventListener('click', handleArrowLeft);
+arrowRight.addEventListener('click', handleArrowRight);
